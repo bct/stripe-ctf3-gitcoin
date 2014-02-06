595fe8eb8e880bb5c6a4cd4234198c8fa8c4b4d0
Brendan Taylor <whateley@gmail.com>
2014-02-06 12:17:25 -0700

    clean up in preparation for release

--- gitcoin.c
+++ gitcoin.c
@@ -40,6 +40,9 @@ int main(int argc, char *argv[]) {
 
   header[0] = 0;
 
+  // the nice thing about a git object is that the bit we can control (the
+  // commit message) comes last, so we can generate a fixed header here and 
+  // just append the generated nonce at the end.
   strcat(header, head_tree);
   strcat(header, tree);
   strcat(header, head_parent);
@@ -49,27 +52,34 @@ int main(int argc, char *argv[]) {
 
   int object_length = strlen(header) + 10;
 
+  // a git object has a header containing the type of the object type and its
+  // length in bytes. it took forever to figure this out.
   sprintf(object_header, "%s%d", head_object, object_length);
 
   fprintf(stderr, "%s\n", header);
 
   int counter = atoi(initial_counter);
 
-  OpenSSL_add_all_digests();
-
+  // this is used in a select call that times out immediately to see if our
+  // parent is still listening. there's probably a better way to do this.
   fd_set the_fd_set;
   struct timeval timeout;
 
   timeout.tv_sec = 0;
   timeout.tv_usec = 1;
 
+  // start initializing our OpenSSL stuff
   unsigned char result_sha1[21];
   char result_sha1_hex[41];
 
   char nonce[11];
 
+  OpenSSL_add_all_digests();
+
   const EVP_MD *md = EVP_sha1();
 
+  // initialize a base message digest context containing the fixed part of the
+  // object.
   EVP_MD_CTX *basectx = EVP_MD_CTX_create();
   basectx = EVP_MD_CTX_create();
   EVP_DigestInit_ex(basectx, md, NULL);
@@ -77,6 +87,7 @@ int main(int argc, char *argv[]) {
   EVP_DigestUpdate(basectx, object_header, strlen(object_header) + 1);
   EVP_DigestUpdate(basectx, header, strlen(header));
 
+  // initialize a message digest context to copy the base context into.
   EVP_MD_CTX mdctx;
   EVP_MD_CTX_init(&mdctx);
   EVP_DigestInit_ex(&mdctx, md, NULL);
@@ -87,35 +98,46 @@ int main(int argc, char *argv[]) {
     sprintf(nonce, "%010x", counter);
 
     if(counter % 0x07ffff == 0) {
+      // every once in a while print a status
+
       fprintf(stderr, "%s\n", nonce);
 
       FD_ZERO(&the_fd_set);
       FD_SET(STDOUT_FILENO, &the_fd_set);
 
+      // check if the parent is still listening
       if(select(STDOUT_FILENO + 1, &the_fd_set, NULL, NULL, &timeout)) {
         // our parent is done with us, we should exit
         exit(0);
       }
     }
 
+    // copy the base context into the actual message digest we're going to use
+    // this is faster than redigesting the fixed header every time we loop.
     EVP_MD_CTX_copy_ex(&mdctx, basectx);
 
+    // update the message digest with our nonce.
     int md_len;
     EVP_DigestUpdate(&mdctx, nonce, 10);
     EVP_DigestFinal_ex(&mdctx, result_sha1, &md_len);
 
+    // convert our SHA1 into a hex string and compare it with the difficulty
+    // (I imagine it would be faster to convert the difficulty to an integer and
+    // compare that instead)
     sha1_to_hex(result_sha1, result_sha1_hex);
 
     if(strcmp(result_sha1_hex, difficulty) < 0) {
-//    if(counter == 1) {
       // we found something!
       fprintf(stderr, "######## I've got something, sir! ########\n");
 
+      // send the result back to our parent on stdout
       puts(result_sha1_hex);
       printf(header);
       printf(nonce);
 
       exit(0);
     }
+
+    // keep looking!
   }
 }
--- miner_c.rb
+++ miner_c.rb
@@ -1,59 +1,60 @@
 #!/usr/bin/env ruby
 
-require 'digest/sha1'
+PUBLIC_USERNAME = "user-jbdufo2q"
+GITCOIN_REPO_PATH = "./current-round"
+
+WORKER_COUNT = 3
+WORKER_SEPARATION = 100_000_000_000
+
+
+## --- config ends here --
 
 # Set up repo
-# I'm assuming it's already pulled here
-local_path = "./current-round"
-Dir.chdir local_path
+# I'm assuming it's already cloned here
+Dir.chdir GITCOIN_REPO_PATH
 
-PUBLIC_USERNAME = "user-jbdufo2q"
+def add_gitcoin_to_ledger
+  ledger_contents = File.read "LEDGER.txt"
 
-def prepare_index()
-    system "perl -i -pe 's/(#{PUBLIC_USERNAME}: )(\d+)/$1 . ($2+1)/e' LEDGER.txt"
-    
-    ledger_contents = File.read "LEDGER.txt"
+  File.open("LEDGER.txt", "w") do |ledger|
+    matcher = /#{PUBLIC_USERNAME}: (\d+)/
+    match = ledger_contents.match(matcher)
 
-    File.open("LEDGER.txt", "w") do |ledger|
-      matcher = /#{PUBLIC_USERNAME}: (\d+)/
-      match = ledger_contents.match(matcher)
-
-      if match
-        ledger_contents.gsub! matcher, "#{PUBLIC_USERNAME}: #{match[1].to_i + 1}"
-        ledger.puts ledger_contents
-      else
-        ledger.puts ledger_contents
-        ledger.puts "#{PUBLIC_USERNAME}: 1"
-      end
+    if match
+      # modify our line in the ledger
+      ledger_contents.gsub! matcher, "#{PUBLIC_USERNAME}: #{match[1].to_i + 1}"
+      ledger.puts ledger_contents
+    else
+      # append our line to the ledger
+      ledger.puts ledger_contents
+      ledger.puts "#{PUBLIC_USERNAME}: 1"
     end
+  end
+end
 
-    puts File.read("LEDGER.txt")
+def prepare_index
+  add_gitcoin_to_ledger
 
-    system "git add LEDGER.txt"
+  puts File.read("LEDGER.txt")
+
+  system "git add LEDGER.txt"
 end
 
 def success! sha1, commit
   puts "success!!!!"
-
-
   puts
   puts "Mined a Gitcoin with commit id: #{sha1}"
 
-  cmd = "git hash-object -t commit --stdin -w"
-
-  IO.popen(cmd, "w") do |io|
-    io.print commit
-  end
+  # write this commit object to the repository
+  git_write_commit(commit)
 
   puts "resetting"
  
-  system "git reset --hard #{sha1} > /dev/null"
+  git_reset sha1
 
   print "pushing... "
 
-  result = system "git push origin master"
-
-  if result
+  if git_push
     puts "succeeded!"
   else
     puts "failed :("
@@ -72,8 +73,8 @@ def solve
 
   readers = []
 
-  3.times do |i|
-    initial_counter = i * 100_000_000_000
+  WORKER_COUNT.times do |i|
+    initial_counter = i * WORKER_SEPARATION
 
     rd, wr = IO.pipe
 
@@ -83,6 +84,7 @@ def solve
 
       readers << rd
 
+      # we don't want our children to be zombies, and we don't care about their exit status.
       Process.detach(pid)
     else
       # in the child
@@ -105,42 +107,55 @@ def solve
       # lol portability and robustness
       commit = rs[0].read + rs[0].read
 
-      File.open("last_commit.txt", "w") { |f| f.print commit }
-      
       success! sha1, commit
 
-      fetch
+      git_fetch
 
       break
     else
       puts "select timed out"
 
+      # if the remote changed then someone mined a coin before us (or the round ended)
       check = `git fetch 2>&1`
 
       if check.match /master/
+        # we'll have to restart
         puts "the world moved beneath us :("
         break
       end
     end
   end
 
+  # close our result channels, the children we spawned will die when they notice.
   readers.each do |rdr|
     rdr.close
   end
 end
 
-def fetch
-    system "git fetch origin master >/dev/null 2>/dev/null"
+def git_fetch
+  system "git fetch origin master >/dev/null 2>/dev/null"
 end
 
-def reset()
-    system "git reset --hard origin/master >/dev/null"
+def git_reset(ref)
+  system "git reset --hard #{ref} >/dev/null"
 end
 
-fetch
+def git_push
+  system "git push origin master"
+end
+
+def git_write_commit(commit)
+  cmd = "git hash-object -t commit --stdin -w"
+
+  IO.popen(cmd, "w") do |io|
+    io.print commit
+  end
+end
+
+git_fetch
 
 loop do
-  reset
+  git_reset "origin/master"
   prepare_index
   solve
 end
